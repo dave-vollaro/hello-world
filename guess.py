c12f05b07fc875ff1b9c2227c818af84919dd543
Dave Vollaro <45641456+dave-vollaro@users.noreply.github.com>
2020-07-10 11:51:12 -0400

    Add files via upload

---
+++ guess.py
@@ -0,0 +1,16 @@
+#!/bin/csh
+# guess_game
+
+import random
+n=random.randint(1,99)
+guess=int(input('Enter an integer from 1 to 99 '))
+while n != guess:
+    if guess < n:
+        print('too low')
+        guess=int(input('Enter an integer from 1 to 99 '))
+    elif guess > n:
+        print('too high')
+        guess=int(input('Enter an integer from 1 to 99 '))
+    else:
+        print('you guessed it the number is ',guess)
+        break
